538b261793b91fcdaa8e1a2877bafc8dfe9cc30d
lksad <it20605902@my.sliit.lk>
2023-07-02 18:50:52 +0530

    add controllers for loging and signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
     @Post('/signup')
     signUp(@Body() SignUpDto: SignUpDto): Promise<{ token:string } > {
         return this.authService.signUp(SignUpDto);
+    }
+
+    @Get('login')
+    login(@Body() loginDto: LoginDto): Promise<{ token:string }> {
+        return this.authService.login(loginDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
 import * as bcrypt from 'bcryptjs'
 import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -28,5 +29,26 @@ export class AuthService {
         const token = this.jwtService.sign({ id: user._id})
 
         return { token }
+    }
+
+    async login(loginDto: LoginDto): Promise<{ token: string }> {
+        const { email, password } = loginDto;
+
+        const user = await this.userModel.findOne({ email })
+
+        if(!user) {
+            throw new UnauthorizedException('Invalid email or password')
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordMatched){
+            throw new UnauthorizedException('Invalid email or password')
+        }
+
+        const token = this.jwtService.sign({id: user._id})
+
+        return { token };
+    
     }
 }
