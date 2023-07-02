f95df87bca57389de53d7be6c382a9223d7b7854
lksad <it20605902@my.sliit.lk>
2023-07-02 18:33:46 +0530

    add signup dto

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,20 @@
+import {IsEnum, IsNotEmpty, IsNumber, IsString, IsEmail, MinLength} from 'class-validator';
+
+
+export class SignUpDto {
+
+@IsNotEmpty()
+@IsString()
+readonly name: string;
+
+@IsNotEmpty()
+@IsEmail({},{message: "Please enter correct email"})
+readonly email: string;
+
+@IsNotEmpty()
+@IsNumber()
+@MinLength(6)
+readonly password: string;
+
+}
+
