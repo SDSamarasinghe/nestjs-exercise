1ddbeb381321223bca806664d03b6aa25323728b
lksad <it20605902@my.sliit.lk>
2023-07-02 17:52:02 +0530

    add auth module and add user schema

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import { SchemaFactory, Prop, Schema } from "@nestjs/mongoose";
+
+
+@Schema({
+    timestamps: true,
+})
+
+export class User {
+    @Prop()
+    name: string
+    @Prop({ unique : [true, 'Duplicate email entered']})
+    email: string
+
+    @Prop()
+    password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
