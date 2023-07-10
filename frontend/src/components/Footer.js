0534dd13c6e89fb8868bc1b447546e4e97dbb8b1
SDSamarasinghe <it20605902@my.sliit.lk>
2023-07-10 12:10:04 +0530

    implemented AddBook option to frontend.

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div style={{ backgroundColor: "#4E5180" }}>
+      <footer className="page-footer font-small  teal py-4 text-white">
+        <div className="container-fluid text-center text-md-left">
+          <div className="row d-flex py-4 justify-content-around">
+            <div className="col-md-3 mt-md-0 mt-3">
+              <h5 className="text-uppercase font-weight-bold">Navigation</h5>
+             
+              <br />
+           
+              <br />
+            
+              <br />
+            
+              <br />
+             
+              <br />
+            </div>
+
+            <div className="col-md-3 mt-md-0 mt-3">
+              <h5 className="text-uppercase font-weight-bold">Navigation</h5>
+             
+              <br />
+             
+              <br />
+            
+              <br />
+           
+             
+              <br />
+            </div>
+
+            <div className="col-md-5 mb-md-0 mb-3 text-start">
+              <h5 className="text-uppercase font-weight-bold">About</h5>
+              <small>
+              
+              </small>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
